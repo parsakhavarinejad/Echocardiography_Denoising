a98adb1c1550cb209d40208bd08553ddd0f33058
parsakhavarinejad <parsa.khavarinejad@gmail.com>
2024-04-09 12:39:20 +0330

    added utils, data

---
+++ utils.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+
+def crop_image(file):
+    center_horizontal = file.shape[2]
+    center_vertical = file.shape[1]
+
+    return file[:, center_vertical // 6:center_vertical * 3 // 4, center_horizontal // 4:center_horizontal * 3 // 4, 0]
+
+
+def save_cropped_image(image_matrix, destination):
+    if image_matrix.dtype != 'uint8':
+        image_matrix = (image_matrix * 255).astype('uint8')
+
+    for y in range(image_matrix.shape[0]):
+        image = Image.fromarray(image_matrix[y, :, :])
+        image.save(os.path.join(destination, f"image_{y}.jpg"))
